e33c3d5368ec281f3a141b870bb8db7e45fe83fa
Martin Chakarov <martin.d.chakarov@gmail.com>
2022-07-15 22:36:04 +0300

    add actor

---
+++ models/staging/hub_actor.sql
@@ -0,0 +1,21 @@
+with raw_data as (
+    select
+        *
+    from
+        {{ source('dvdrental', 'actor') }}  
+),
+
+final as (
+    select
+        md5(actor_id::varchar) as hk_actor_id,
+        actor_id,
+        current_timestamp as load_date,
+        'Postgres' as record_source
+    from
+        raw_data
+)
+
+select
+    *
+from
+    final
