a165bad37684ff5918f43d247b2f2a89688d86b4
Martin Chakarov <martin.d.chakarov@gmail.com>
2022-07-17 14:09:13 +0300

    update customer, staff, store sats with address data

--- models/staging/sat_store.sql
+++ models/staging/sat_store.sql
@@ -5,14 +5,59 @@ with raw_data as (
         {{ source('dvdrental', 'store') }}
 ),
 
+addresses as (
+    select
+        *
+    from
+        {{ source('dvdrental', 'address') }}
+    where
+        address_id in (select distinct address_id from raw_data)
+),
+
+cities as (
+    select
+        *
+    from
+        {{ source('dvdrental', 'city') }}
+    where
+        city_id in (select distinct city_id from addresses)
+),
+
+countries as (
+    select
+        *
+    from
+        {{ source('dvdrental', 'country') }}
+    where
+        country_id in (select distinct country_id from cities)
+),
+
+full_addresses as (
+    select
+        *
+    from
+        addresses
+        left join cities
+        on addresses.city_id = cities.city_id
+        left join countries
+        on cities.country_id = countries.country_id
+),
+
 final as (
     select
-        md5(store_id::varchar) as hk_store_id,
-        'Store ' || store_id as store_name,
+        md5(raw_data.store_id::varchar) as hk_store_id,
         current_timestamp as load_date,
-        'System Generated' as record_source
+        full_addresses.address,
+        full_addresses.district,
+        full_addresses.postal_code,
+        full_addresses.phone,
+        full_addresses.city,
+        full_addresses.country,
+        'Postgres' as record_source
     from
         raw_data
+        left join full_addresses
+        on raw_data.address_id = full_addresses.address_id
 )
 
 select
